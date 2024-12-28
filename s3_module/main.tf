450bb352b162a076ed315ab27e0a6457faa0530e
Darren Rabbitt <darrenrabbitt@gmail.com>
2024-12-29 00:06:08 +0100

    apply fixes

--- s3_module/main.tf
+++ s3_module/main.tf
@@ -4,10 +4,32 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-123456"
-  instance_type = "" # Invalid instance type (edit)
+  instance_type = "t2.micro" # Updated to a valid instance type
 }
 
 resource "aws_s3_bucket" "example" {
   bucket = "my-bucket"
-  acl    = "public-read" # Public access
+  acl    = "private" # Changed ACL to private for enhanced security
+}
+
+resource "aws_s3_bucket_policy" "example_policy" {
+  bucket = aws_s3_bucket.example.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Deny"
+        Action   = "s3:*"
+        Resource = "${aws_s3_bucket.example.arn}/*"
+        Principal = {
+          AWS = "*"
+        }
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" : "false"
+          }
+        }
+      }
+    ]
+  })
 }
